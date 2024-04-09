6082706b5ab0e824fe0a301578bf478e19d2ea4c
joosun7227 <96054361+joosun7227@users.noreply.github.com>
2024-04-09 14:49:14 +0900

    php_study2

---
+++ class1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h6>PHP_study.<h6>
+  <?php
+    echo "안녕하세요";
+  //변수 
+  $a = 1;
+
+
+  ?>
+  <script>
+    document.write("안녕하세요")
+    let a = 1;
+  </script>
+</body>
+</html>
