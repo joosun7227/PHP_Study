b0a556e83c61edecea71afa98f8384c25a27e905
joosun7227 <96054361+joosun7227@users.noreply.github.com>
2024-04-17 07:10:29 +0900

    Update 8.비교연산자.php

---
+++ 8.비교연산자.php
@@ -0,0 +1,30 @@
+<?php
+// 비교 연산자
+
+$a = 10;
+$b = "10";
+$c = 11;
+
+
+if ($a == $b){
+  echo "a와 b는 같다.";
+} else {
+  echo "a와 b는 다르다.";
+}
+
+print "<br>";
+
+if ($a == $c){
+  echo "a와 c는 같다.";
+} else {
+  echo "a와 c는 다르다.";
+}
+
+print "<br>";
+
+if ($a == $c){
+  echo "a와 c는 같다.";
+} else {
+  echo "a와 c는 다르다.";
+}
+?>
